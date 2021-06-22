3296927131c77a55fd9c75b0003a5f4035730305
freebear1254 <freebear1254@gmail.com>
2021-06-22 14:33:35 +0900

    practice nodejs

--- nodejs/main.js
+++ nodejs/main.js
@@ -8,7 +8,17 @@ var app = http.createServer(function (request, response) {
   const queryData = url.parse(_url, true).query;
   const pathName = url.parse(_url, true).pathname;
   let title = queryData.id;
-  function createTemplet(title, data, fileList) {
+  const create = `<a href = "/create">create</a> `;
+  const update = `
+  <a href = "/update?id=${title}">update</a> 
+  <form action="/delete_process" method="POST">
+    <input type="hidden" name="title" value ="${title}">
+    <input type="submit"  value ="delete">   
+</form>
+  `;
+
+
+  function createTemplet(title, data, fileList, control) {
     templete = `
     <!doctype html>
     <html>
@@ -20,35 +30,37 @@ var app = http.createServer(function (request, response) {
     <h1><a href="/">${title}</a></h1>
     <ol>
     ${fileList}
-    </ol>
-    <a href = "/create">create</a>
+    </ol>        
+    ${control}  
     <h2>${title}</h2>
     <p>${data}</p>
     </body>
     </html>        
     `;
     response.writeHead(200);
-    response.end(templete); 
+
 
   }
 
   if (pathName === '/') {
     fs.readFile(`data/${title}`, `utf8`, function (err, data) {
+      control = update;
       if (title === undefined) {
         title = "Welcome";
         data = `"Hello Node.js"`
+        control = create;
       }
       fs.readdir(`./data`, (err, fileName) => {
         let fileList = '';
         for (i = 0; i < fileName.length; i++) {
           fileList = fileList + `<li><a href = '/?id=${fileName[i]}'>${fileName[i]}</a></li>`
         }
-        createTemplet(title, data, fileList);
-        response.end(templete); 
+        createTemplet(title, data, fileList, control);
+        response.end(templete);
       });
     })
-
-  } else if (pathName === '/create') {
+  }
+  else if (pathName === '/create') {
     title = "Create";
     data = `
     <form action="http://localhost:3000/create_process" method="POST">
@@ -56,16 +68,19 @@ var app = http.createServer(function (request, response) {
     <p><textarea name="content" id="" cols="30" rows="10" placeholder="여기"></textarea></p>
     <p><input type="submit" value="submit"></p>
     </form>
-        `
-
+    `
     fs.readdir(`./data`, (err, fileName) => {
       let fileList = '';
+      control = "";
       for (i = 0; i < fileName.length; i++) {
         fileList = fileList + `<li><a href = '/?id=${fileName[i]}'>${fileName[i]}</a></li>`
       }
-      createTemplet(title, data, fileList);
+      createTemplet(title, data, fileList, control);
+      response.end(templete);
     });
-  } else if (pathName === '/create_process') {
+
+  }
+  else if (pathName === '/create_process') {
     var body = '';
     request.on('data', function (data) {
       body += data;
@@ -74,19 +89,82 @@ var app = http.createServer(function (request, response) {
     let content = '';
 
     request.on('end', function () {
-      const post = qs.parse(body);
+      let post = qs.parse(body);
       title = post.title;
       content = post.content;
 
-      
       fs.writeFile(`data/${title}`, `${content}`, function (err) {
-        response.writeHead(302,{Location : `/?id=${title}`});
+        response.writeHead(302, { Location: `/?id=${title}` });
         response.end(`title : ${title} // content : ${content}`);
         console.log(err);
-      })
+      });
 
     })
   }
+  else if (pathName === '/update') {
+
+    fs.readFile(`data/${title}`, `utf8`, function (err, data) {
+      control = '';
+
+      data = `
+    <form action="http://localhost:3000/update_process" method="POST">
+    <p><input type="hidden" name="id" id="" value="${title}" ></p>
+    <p><input type="text" name="title" id="" placeholder="${title}" value ="${title}"></p>
+    <p><textarea name="content" id="" cols="30" rows="10" placeholder="${data}" value="${data}">${data}</textarea></p>
+    <p><input type="submit" value="submit"></p>
+    </form>
+    `
+      fs.readdir(`./data`, (err, fileName) => {
+        let fileList = '';
+        for (i = 0; i < fileName.length; i++) {
+          fileList = fileList + `<li><a href = '/?id=${fileName[i]}'>${fileName[i]}</a></li>`
+        }
+
+        createTemplet(title, data, fileList, control);
+        response.end(templete);
+      });
+    })
+  } else if (pathName === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var content = post.content;
+
+      fs.rename(`data/${id}`, title, function (err) {
+        fs.writeFile(`data/${title}`, content, 'utf8', function (error) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end(`title : ${title} // content : ${content}`);
+          console.log(error);
+        })
+        console.log(err);
+      });
+     });
+  }
+  else if(pathName === '/delete_process'){   
+
+    var body ='';
+    request.on('data',function(data){
+      body += data;
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      var title = post.title;
+
+      fs.unlink(`data/${title}`,function(err){
+        console.log(err);
+        response.writeHead(302,{location : `/`});
+        response.end();
+      });
+    })
+
+
+  }
+
 
   else {
     response.writeHead(404);
@@ -94,6 +172,5 @@ var app = http.createServer(function (request, response) {
   }
 
 });
-
 
 app.listen(3000);
